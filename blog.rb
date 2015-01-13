86f16ce165025ced08e202de6c3f40333044330b
Gabe Pressman <pressman4747@gmail.com>
2015-01-13 16:35:20 -0500

    first

---
+++ blog.rb
@@ -0,0 +1,20 @@
+# Create Blog class
+# Create front page
+#post class
+#title
+#date
+#text
+# Post title 1
+# **************
+# Post 1 text
+# ----------------
+
+class Blog
+	def front_page
+		puts "This is Gabe's blog"
+	end
+end
+
+blog= Blog.new
+
+blog.front_page

046754eafad785b6a4cc5fe69da7bbba616b55a6
Gabe Pressman <pressman4747@gmail.com>
2015-01-13 17:21:22 -0500

    finished?

--- blog.rb
+++ blog.rb
@@ -8,13 +8,63 @@
 # **************
 # Post 1 text
 # ----------------
+class Post
 
-class Blog
-	def front_page
-		puts "This is Gabe's blog"
+	def initialize(title, date, post)
+		@title=title
+		@date=date
+		@post=post	
+		@postsplit= "*******************"
+		@postend= "__________________"
+
+	end
+	def makePost
+		puts @title
+		puts @postsplit
+		puts @post 
+		puts @date
+		puts @postend
 	end
 end
 
-blog= Blog.new
 
-blog.front_page
+
+class Blog
+	def initialize(array)
+		@list = array
+	end
+	def front_page
+		puts "This is Gabe's blog"
+		@list
+	
+	end
+	def add_post 
+
+
+	end
+
+
+
+end
+
+
+
+
+
+post=Post.new("First post", "1/13", "post content" )
+post2=Post.new("second post", "1/13", "post content" )
+post3=Post.new("third post", "1/13", "post content" )
+post4=Post.new("fourth post", "1/13", "post content" )
+
+uglyWay = [post.makePost,post2.makePost,post3.makePost, post4.makePost]
+blog= Blog.new(uglyWay)
+
+
+# blog.add_post(post)
+# blog.add_post(post)
+# blog.add_post(post)
+# blog.add_post(post)
+# blog.add_post(post)
+# blog.front_page
+
+
